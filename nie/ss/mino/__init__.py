357c43d0b21b4ab6eb0f30a17515ceb3a93217c5
ponponon <1729303158@qq.com>
2022-07-17 19:47:17 +0800

    feature: 开始支持文件读取

--- nie/ss/mino/__init__.py
+++ nie/ss/mino/__init__.py
@@ -1,6 +1,34 @@
 from nie.aliases import pyminio
 from nie.loggers import logger
 import io
+from typing import overload
+
+
+from typing import overload, TypeAlias, Literal
+
+READ_TEXT_MODE: TypeAlias = Literal[
+    'r',
+]
+# write
+WRITE_TEXT_MODE: TypeAlias = Literal[
+    "w",
+]
+
+APPEND_TEXT_MODE: TypeAlias = Literal[
+    "a",
+]
+
+READ_STREAM_MODE: TypeAlias = Literal[
+    "rb",
+]
+# write
+WRITE_STREAM_MODE: TypeAlias = Literal[
+    "wb",
+]
+
+APPEND_STREAM_MODE: TypeAlias = Literal[
+    "ab",
+]
 
 
 class MinioClient:
@@ -9,9 +37,13 @@ class MinioClient:
         self.access_key = access_key
         self.secret_key = secret_key
         self.bucket_name = bucket_name
+        
+    # def open(self, file_path: str, mode: str, encoding: str = 'utf-8'):
+    #     raise Exception(f'无效的 mode: {mode}')
 
-    def open(self, file_path: str, mode: str, encoding=str):
-        self.file_path=file_path
+    @overload
+    def open(self, file_path: str, mode: WRITE_TEXT_MODE, encoding: str = 'utf-8'):
+        self.file_path = file_path
         if mode == 'w':
             self.conn = pyminio.Minio(
                 self.end_point,
@@ -19,10 +51,47 @@ class MinioClient:
                 secret_key=self.secret_key,
                 secure=False
             )
-        return MinioFileWriter(self)
+        return MinioTextFileWriter(self)
+
+    @overload
+    def open(self, file_path: str, mode: READ_TEXT_MODE, encoding: str = 'utf-8'):
+        self.file_path = file_path
+        assert mode == 'r'
+        self.conn = pyminio.Minio(
+            self.end_point,
+            access_key=self.access_key,
+            secret_key=self.secret_key,
+            secure=False
+        )
+        return MinioTextFileReader(self)
+
+    @overload
+    def open(self, file_path: str, mode: WRITE_STREAM_MODE):
+        self.file_path = file_path
+        if mode == 'w':
+            self.conn = pyminio.Minio(
+                self.end_point,
+                access_key=self.access_key,
+                secret_key=self.secret_key,
+                secure=False
+            )
+        return MinioStreamFileWriter(self)
+
+    
 
 
-class MinioFileWriter:
+class MinioBaseFileWriter:
+    def __init__(self, client: MinioClient) -> None:
+        self.client = client
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args, **kwargs):
+        pass
+
+
+class MinioTextFileWriter(MinioBaseFileWriter):
     def __init__(self, client: MinioClient) -> None:
         self.client = client
 
@@ -45,9 +114,47 @@ class MinioFileWriter:
         logger.debug(kwargs)
 
 
-class MinioFileReader:
-    def __init__(self) -> None:
+class MinioStreamFileWriter(MinioBaseFileWriter):
+    def __init__(self, client: MinioClient) -> None:
+        self.client = client
+
+    def write(self, stream: bytes) -> int:
+        self.client.conn.put_object(
+            bucket_name=self.client.bucket_name,
+            object_name=self.client.file_path,
+            data=io.BytesIO(stream),
+            length=len(stream),
+        )
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args, **kwargs):
+        logger.debug(args)
+        logger.debug(kwargs)
+
+
+class MinioBaseFileReader:
+    def __init__(self, client: MinioClient) -> None:
+        self.client = client
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args, **kwargs):
         pass
 
-    def read(self, content: str | bytes):
-        pass
+
+class MinioTextFileReader(MinioBaseFileReader):
+
+    def read(self) -> str:
+        object = self.client.conn.get_object(
+            bucket_name=self.client.bucket_name,
+            object_name=self.client.file_path,
+        )
+        object.data
+
+        from nie.loggers import logger
+        logger.debug(type(object))
+        logger.debug(type(object.data))
+        logger.debug(type(object.stream))
--- run.py
+++ run.py
@@ -3,18 +3,6 @@ from mark import BASE_DIR
 from nie.ss.mino import MinioClient
 from loguru import logger
 
-# with nopen(BASE_DIR/'') as file:
-#     content = file.read()
-
-# with open(BASE_DIR/'001.jpg', 'rb') as file:
-
-#     content = file.read()
-
-
-# with open(BASE_DIR/'001.txt', 'r') as file:
-
-#     content = file.read()
-
 
 client = MinioClient(
     end_point='192.168.31.245:9000',
@@ -26,3 +14,12 @@ client = MinioClient(
 with client.open('test/001.txt', 'w', encoding='utf-8') as file:
     logger.debug(f'开始写入')
     file.write('你好呀')
+    
+    
+# with client.open('test/001.txt', 'r', encoding='utf-8') as file:
+#     logger.debug(f'开始读取')
+#     file.read()
+
+# with client.open('test/001.jpg', 'wb') as file:
+#     logger.debug(f'开始写入')
+#     file.write('你好呀')
