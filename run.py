5faaf94534d22f6cde999202d6d465cb4c3e7fdc
ponponon <1729303158@qq.com>
2022-08-11 12:43:54 +0800

    feature:

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
 from nie import nopen
 from mark import BASE_DIR
-from nie.ss.mino import MinioClient
+from nie.ss.minio import MinioClient
 from loguru import logger
 from typing import overload
 from typing import Generator
+from loguru import logger
 
 client = MinioClient(
     end_point='192.168.31.245:9000',
@@ -12,17 +13,15 @@ client = MinioClient(
     bucket_name='nie',
 )
 
-# with client.open('test/001.txt', 'w', encoding='utf-8') as file:
-#     logger.debug(f'开始写入')
-#     file.write('你好呀哈哈')
+with client.open('test/001.txt', 'w', encoding='utf-8') as file:
+    logger.debug(f'开始写入')
+    file.write('你好呀哈哈')
 
 
 with client.open('test/001.txt', 'r', encoding='utf-8') as file:
     logger.debug(f'开始读取')
-    for text in file.readline():
+    logger.debug(file.read())
 
-        logger.debug(text)
+has_obj = client.os.path.exists('test/001.txt')
 
-# with client.open('test/001.jpg', 'wb') as file:
-#     logger.debug(f'开始写入')
-#     file.write('你好呀')
+logger.debug(has_obj)
